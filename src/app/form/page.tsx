45f200ff73f905a9c9f6e3bddcf072a1bcc87408
Himanshu Choudhary <raptordev21@gmail.com>
2024-01-13 20:22:57 +0530

    shadcn ui setup added

---
+++ src/app/form/page.tsx
@@ -0,0 +1,10 @@
+import * as zod from "zod"
+import { useForm } from "react-hook-form"
+
+export default function Form() {
+  return (
+    <div className="flex flex-col justify-start items-center w-3/5 mx-auto bg-slate-100 my-4 p-4 rounded">
+      <h1 className="text-lg text-cyan-500">Form</h1>
+    </div>
+  )
+}
