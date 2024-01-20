c5ee177b7f2673527f3bb85bc8b89e040bd7f6e6
Himanshu Choudhary <raptordev21@gmail.com>
2024-01-20 19:53:05 +0530

    landing page style updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="">
-      <Link href="/form">Form</Link>
+    <main className="py-8">
+      <section className="mx-auto p-2 w-3/4 bg-gray-100 rounded">
+        <div className="mb-2">
+          <Input placeholder="Search" />
+        </div>
+        <div className="grid grid-cols-2 gap-2">
+          <Card>
+            <CardHeader>
+              <CardTitle><Link href="/form">Form</Link></CardTitle>
+              <CardDescription>Complete Form</CardDescription>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader>
+              <CardTitle><Link href="/table">Table</Link></CardTitle>
+              <CardDescription>Complete Table</CardDescription>
+            </CardHeader>
+          </Card>
+        </div>
+      </section>
     </main>
   )
 }
