1550de7fba2b095f411bd453a927ddaba2f05da5
Himanshu Choudhary <raptordev21@gmail.com>
2024-01-20 18:41:55 +0530

    working form added

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -2,41 +2,143 @@
 import * as z from "zod"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import {
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+  Select,
+} from "@/components/ui/select"
 
-const formSchema = z.object({
-  emailAddress: z.string().email()
-})
+const formSchema = z
+  .object({
+    emailAddress: z.string().email(),
+    password: z.string().min(3),
+    passwordConfirm: z.string(),
+    accountType: z.enum(["personal", "company"]),
+    companyName: z.string().optional(),
+  })
+  .refine(
+    (data) => {
+      return data.password === data.passwordConfirm
+    },
+    {
+      message: "Passwords do not match",
+      path: ["passwordConfirm"],
+    }
+  )
+  .refine(
+    (data) => {
+      if (data.accountType === "company") {
+        return !!data.companyName
+      }
+      return true
+    },
+    {
+      message: "Company Name is required",
+      path: ["companyName"],
+    }
+  )
 
-export default function FormShadcn() {
+export default function Home() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      emailAddress: ""
-    }
+      emailAddress: "",
+      password: "",
+      passwordConfirm: "",
+      companyName: "",
+    },
   })
 
-  const formSubmit = () => { }
+  const accountType = form.watch("accountType")
+
+  const handleSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log({ values })
+  }
+
 
   return (
     <div className="flex flex-col justify-start items-center w-3/5 mx-auto bg-slate-100 my-4 p-4 rounded">
       <h1 className="text-lg text-cyan-500">Form</h1>
       <div className="w-full p-4">
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(formSubmit)} className="max-w-md mx-auto w-full flex flex-col gap-4">
-            <FormField control={form.control} name="emailAddress" render={(field) => (
+          <form onSubmit={form.handleSubmit(handleSubmit)} className="max-w-md mx-auto w-full flex flex-col gap-4">
+            <FormField control={form.control} name="emailAddress" render={({ field }) => (
               <FormItem>
-                <FormLabel>Email</FormLabel>
+                <FormLabel>Email address</FormLabel>
                 <FormControl>
-                  <Input placeholder="Email" type="email" {...field} />
+                  <Input placeholder="Email address" type="email" {...field} />
                 </FormControl>
-                <FormDescription>abc@abc.com</FormDescription>
+                <FormDescription>john.doe@gmail.com</FormDescription>
                 <FormMessage />
               </FormItem>
-            )} />
-            <Button type="submit">Submit</Button>
+            )}
+            />
+            <FormField control={form.control} name="accountType" render={({ field }) => (
+              <FormItem>
+                <FormLabel>Account type</FormLabel>
+                <Select onValueChange={field.onChange}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select an account type" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="personal">Personal</SelectItem>
+                    <SelectItem value="company">Company</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+            />
+            {accountType === "company" && (
+              <FormField control={form.control} name="companyName" render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Company Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Company Name" type="text" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+              />
+            )}
+            <FormField control={form.control} name="password" render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="Password" type="password" autoComplete="on" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+            />
+            <FormField control={form.control} name="passwordConfirm" render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password Confirm</FormLabel>
+                <FormControl>
+                  <Input placeholder="Password Confirm" type="password" autoComplete="on" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+            />
+            <div className="flex flex-row justify-center items-center">
+              <Button type="submit" className="w-36">Submit</Button>
+            </div>
           </form>
         </Form>
       </div>
