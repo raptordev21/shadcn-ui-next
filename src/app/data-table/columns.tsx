bb74fd5afd80b223c77f991ed8bd2b25e933e422
Himanshu Choudhary <raptordev21@gmail.com>
2024-02-15 09:56:46 +0530

    pagination added

---
+++ src/app/data-table/columns.tsx
@@ -0,0 +1,84 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { MoreHorizontal } from "lucide-react"
+
+import { toast } from "sonner"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Payment = {
+  id: string
+  amount: number
+  status: "pending" | "processing" | "success" | "failed"
+  email: string
+}
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"))
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount)
+
+      return <div className="text-right font-medium">{formatted}</div>
+    },
+  },
+  {
+    id: "actions",
+    header: () => <div className="text-left">Actions</div>,
+    cell: ({ row }) => {
+      const payment = row.original // gives row data
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() =>
+              toast(`${payment.email}`, {
+                description: "Sunday, December 03, 2023 at 9:00 AM",
+                action: {
+                  label: "Undo",
+                  onClick: () => console.log("Undo"),
+                },
+              })}>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
+  },
+]
--- src/app/data-table/page.tsx
+++ src/app/data-table/page.tsx
@@ -1,7 +1,90 @@
-export default function DataTableShadcnUI() {
+import { Payment, columns } from "./columns"
+import { DataTable } from "./data-table"
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "55728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed522",
+      amount: 200,
+      status: "processing",
+      email: "mqq@example.com",
+    },
+    {
+      id: "728ed523",
+      amount: 300,
+      status: "success",
+      email: "mww@example.com",
+    },
+    {
+      id: "728ed52355",
+      amount: 300,
+      status: "success",
+      email: "mww@example.com",
+    },
+    {
+      id: "728ed52366",
+      amount: 300,
+      status: "success",
+      email: "mww@example.com",
+    },
+    {
+      id: "728ed52377",
+      amount: 300,
+      status: "success",
+      email: "mww@example.com",
+    },
+    {
+      id: "728ed52388",
+      amount: 300,
+      status: "success",
+      email: "mww@example.com",
+    },
+    {
+      id: "728ed52399",
+      amount: 300,
+      status: "success",
+      email: "mww@example.com",
+    },
+    {
+      id: "728ed52355645",
+      amount: 300,
+      status: "success",
+      email: "mww@example.com",
+    },
+    {
+      id: "728ed5232331",
+      amount: 300,
+      status: "success",
+      email: "mww@example.com",
+    },
+    {
+      id: "728ed524",
+      amount: 400,
+      status: "failed",
+      email: "mdf@example.com",
+    },
+  ]
+}
+
+export default async function DataTableShadcnUI() {
+  const data = await getData()
+
   return (
-    <div>
-      hi
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
     </div>
   )
 }
